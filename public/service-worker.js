907c4efca4c0ad74c4e834ee890f90e772fe298b
Maria Waslick <mariawaslick@gmail.com>
2020-11-04 22:33:02 -0500

    fixed service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,11 +15,6 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (evt) {
-    // pre cache image data
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
-      
     // pre cache all static assets
     evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
