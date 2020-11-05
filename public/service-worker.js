6aebcfc85cc0ed9b31511908c59e5ffef356b00b
Maria Waslick <mariawaslick@gmail.com>
2020-11-04 22:46:06 -0500

    added files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,9 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/styles.css"
+  "/styles.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ]
 
 
